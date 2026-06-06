270476f0bde2b61a9e932c076efe8aceb0227570
Quentin Arragon <arragonquentin@gmail.com>
2026-06-06 22:09:48 +0200

    selectable

--- Assets/Game/Gameplay/Encounters/DialogChoice/DialogChoiceEntryView.cs
+++ Assets/Game/Gameplay/Encounters/DialogChoice/DialogChoiceEntryView.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+using UnityEngine.EventSystems;
+using System;
 
-public class DialogChoiceEntryView : MonoBehaviour
+[RequireComponent(typeof(Selectable))]
+public class DialogChoiceEntryView : MonoBehaviour, ISelectHandler, IDeselectHandler, ISubmitHandler
 {
 
     [SerializeField]
     private Image background = null;
 
+    private DialogChoiceEntry _entry;
+
+    public DialogChoiceEntry Entry => _entry;
+    public event Action<DialogChoiceEntryView> Submitted;
+
     public void Display(DialogChoiceEntry entry)
     {
-
+        _entry = entry;
         background.color = entry.color;
+        transform.localScale = Vector3.one;
     }
+
+    public void OnSelect(BaseEventData _) => transform.localScale = Vector3.one * 1.1f;
+    public void OnDeselect(BaseEventData _) => transform.localScale = Vector3.one;
+    public void OnSubmit(BaseEventData _) => Submitted?.Invoke(this);
 }
--- Assets/Game/Gameplay/Encounters/DialogChoice/DialogChoiceView.cs
+++ Assets/Game/Gameplay/Encounters/DialogChoice/DialogChoiceView.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using System;
 
 public class DialogChoiceView : MonoBehaviour
@@ -8,13 +9,25 @@ public class DialogChoiceView : MonoBehaviour
     [SerializeField]
     private Transform choicesContainer = null;
 
+    public event Action<DialogChoiceEntry> ChoiceSubmitted;
 
     public void DisplayChoices(DialogChoice dialogChoice)
     {
+        DialogChoiceEntryView first = null;
         foreach (var choice in dialogChoice.playerEntries)
         {
             var choiceEntryView = Instantiate(choiceEntryViewPrefab, choicesContainer);
             choiceEntryView.Display(choice);
+            choiceEntryView.Submitted += OnEntrySubmitted;
+            if (first == null) first = choiceEntryView;
         }
+
+        if (first != null)
+            EventSystem.current.SetSelectedGameObject(first.gameObject);
+    }
+
+    private void OnEntrySubmitted(DialogChoiceEntryView view)
+    {
+        ChoiceSubmitted?.Invoke(view.Entry);
     }
 }
