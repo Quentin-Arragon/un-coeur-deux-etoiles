1a83b970490536c34a6e59ddcb6acf9211e3b373
Quentin Arragon <arragonquentin@gmail.com>
2026-06-06 16:21:11 +0200

    intro scene

---
+++ Assets/Scripts/Intro/Intro.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class Intro : MonoBehaviour
+{
+    [SerializeField]
+    private float introDuration = 3f;
+    [SerializeField]
+    private GameObject anyKeyPrompt;
+
+    private bool hasCompletedIntro = false;
+
+
+    IEnumerator Start()
+    {
+        anyKeyPrompt.SetActive(false);
+        yield return new WaitForSeconds(introDuration);
+        anyKeyPrompt.SetActive(true);
+        hasCompletedIntro = true;
+    }
+
+    void Update()
+    {
+        if (hasCompletedIntro && Keyboard.current.anyKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+}
