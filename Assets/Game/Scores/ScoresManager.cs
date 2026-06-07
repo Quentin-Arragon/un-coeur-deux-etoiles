2a7631ac4ccdacf13c553fcf8694a13f5ef29701
Quentin Arragon <arragonquentin@gmail.com>
2026-06-07 11:52:29 +0200

    scores manager

--- Assets/Game/Scores/ScoresManager.cs
+++ Assets/Game/Scores/ScoresManager.cs
@@ -10,19 +10,53 @@ public class ScoresManager : MonoBehaviour
     [SerializeField]
     private GameObject anyKeyPrompt;
 
+    private bool animationFinished;
 
     IEnumerator Start()
     {
         anyKeyPrompt.SetActive(false);
-        yield return new WaitForSeconds(introDuration);
+        yield return StartCoroutine(PlayIntro());
+    }
+
+    IEnumerator PlayIntro()
+    {
+        // TODO: remplacer ce délai par l'animation quand elle sera implémentée.
+        float elapsed = 0f;
+        while (elapsed < introDuration)
+        {
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        FinishIntro();
+    }
+
+    void FinishIntro()
+    {
+        if (animationFinished)
+        {
+            return;
+        }
+        // TODO: forcer l'animation à sa dernière frame quand elle sera implémentée.
+        animationFinished = true;
         anyKeyPrompt.SetActive(true);
     }
 
     void Update()
     {
-        if (Keyboard.current.anyKey.wasPressedThisFrame)
+        if (!Keyboard.current.anyKey.wasPressedThisFrame)
+        {
+            return;
+        }
+
+        if (animationFinished)
         {
             SceneManager.LoadScene("Intro");
+        }
+        else
+        {
+            // Skip : on stoppe l'intro en cours et on saute directement à la fin.
+            StopAllCoroutines();
+            FinishIntro();
         }
     }
 }
