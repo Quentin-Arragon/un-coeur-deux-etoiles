efd2f648670fc42aab356de07924277499641728
Quentin Arragon <arragonquentin@gmail.com>
2026-06-07 11:16:13 +0200

    sound category

--- Assets/Game/Common/GameSound.cs
+++ Assets/Game/Common/GameSound.cs
@@ -1,9 +1,16 @@
 using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 [Serializable]
 public class GameSound
 {
     public string id;
+
+    [ValueDropdown(nameof(Categories))]
+    public string category;
+
     public AudioClip clip;
+
+    private static readonly string[] Categories = { "trumpet", "drumb", "violin" };
 }
