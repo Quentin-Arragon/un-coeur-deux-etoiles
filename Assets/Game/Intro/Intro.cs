7a72e88a7a8f04cc3a28e70d0a368361cf621c58
Quentin Arragon <arragonquentin@gmail.com>
2026-06-07 12:09:35 +0200

    test amorce

--- Assets/Game/Intro/Intro.cs
+++ Assets/Game/Intro/Intro.cs
@@ -2,28 +2,77 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 
 public class Intro : MonoBehaviour
 {
     [SerializeField]
-    private float introDuration = 3f;
+    private VideoPlayer introVideo;
     [SerializeField]
     private GameObject anyKeyPrompt;
 
+    private bool animationFinished;
 
     IEnumerator Start()
     {
         anyKeyPrompt.SetActive(false);
-        yield return new WaitForSeconds(introDuration);
+
+        introVideo.isLooping = false;
+        introVideo.loopPointReached += OnVideoFinished;
+        introVideo.Play();
+
+        yield return null;
+    }
+
+    void OnVideoFinished(VideoPlayer source)
+    {
+        FinishIntro();
+    }
+
+    void FinishIntro()
+    {
+        if (animationFinished)
+        {
+            return;
+        }
+        animationFinished = true;
         anyKeyPrompt.SetActive(true);
+    }
+
+    void SkipToEnd()
+    {
+        // Saute directement à la dernière frame de la vidéo.
+        if (introVideo.frameCount > 0)
+        {
+            introVideo.frame = (long)introVideo.frameCount - 1;
+        }
+        introVideo.Pause();
+        FinishIntro();
     }
 
     void Update()
     {
-        if (Keyboard.current.anyKey.wasPressedThisFrame)
+        if (!Keyboard.current.anyKey.wasPressedThisFrame)
+        {
+            return;
+        }
+
+        if (animationFinished)
         {
             SceneManager.LoadScene("Gameplay");
+        }
+        else
+        {
+            SkipToEnd();
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (introVideo != null)
+        {
+            introVideo.loopPointReached -= OnVideoFinished;
         }
     }
 }
